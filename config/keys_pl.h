b3bfa1570fceb049ca10cb677f66dd4080f1c809
Ziembski <karolzurawinski@gmail.com>
2026-04-09 18:11:32 +0200

    fix and adjustment

---
+++ config/keys_pl.h
@@ -0,0 +1,46 @@
+// Copyright 2025 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+/*******************************************************************************
+  88888888888 888      d8b                .d888 d8b 888               d8b
+      888     888      Y8P               d88P"  Y8P 888               Y8P
+      888     888                        888        888
+      888     88888b.  888 .d8888b       888888 888 888  .d88b.       888 .d8888b
+      888     888 "88b 888 88K           888    888 888 d8P  Y8b      888 88K
+      888     888  888 888 "Y8888b.      888    888 888 88888888      888 "Y8888b.
+      888     888  888 888      X88      888    888 888 Y8b.          888      X88
+      888     888  888 888  88888P'      888    888 888  "Y8888       888  88888P'
+                                                        888                 888
+                                                        888                 888
+                                                        888                 888
+     .d88b.   .d88b.  88888b.   .d88b.  888d888 8888b.  888888 .d88b.   .d88888
+    d88P"88b d8P  Y8b 888 "88b d8P  Y8b 888P"      "88b 888   d8P  Y8b d88" 888
+    888  888 88888888 888  888 88888888 888    .d888888 888   88888888 888  888
+    Y88b 888 Y8b.     888  888 Y8b.     888    888  888 Y88b. Y8b.     Y88b 888
+     "Y88888  "Y8888  888  888  "Y8888  888    "Y888888  "Y888 "Y8888   "Y88888
+         888
+    Y8b d88P
+     "Y88P"
+*******************************************************************************/
+
+#pragma once
+
+
+#include <dt-bindings/zmk/hid_usage.h>
+#include <dt-bindings/zmk/hid_usage_pages.h>
+#include <dt-bindings/zmk/modifiers.h>
+#include <dt-bindings/zmk/keys.h>
+
+// Aliases
+
+
+#define PL_A     RA(A)         /* ą  U+0105 */
+#define PL_C     RA(C)         /* ć  U+0107 */
+#define PL_E     RA(E)         /* ę  U+0119 */
+#define PL_L     RA(L)         /* ł  U+0142 */
+#define PL_N     RA(N)         /* ń  U+0144 */
+#define PL_O     RA(O)         /* ó  U+00F3 */
+#define PL_S     RA(S)         /* ś  U+015B */
+#define PL_X     RA(X)         /* ź  U+017A */
+#define PL_Z     RA(Z)         /* ż  U+017C */
+
